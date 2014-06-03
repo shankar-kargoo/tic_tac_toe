e7fbc468d1823b9e88964cd9829034d2c1fc92d0
Shankar Narayan <shankar@kargoo.com>
2014-06-02 19:40:52 -0700

    game working

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -17,6 +17,7 @@ def draw_board(b)
  	puts 	"#{b[4]} |#{b[5]} |#{b[6]}	"
  	puts 	"--+--+--"
  	puts 	"#{b[7]} |#{b[8]} |#{b[9]}	"
+ 	return b
 end
 
 def empty_boxes(b)
@@ -27,49 +28,33 @@ end
 def players_turn(board)
 	puts "Please enter a value between #{empty_boxes(board)}"
 	y = gets.chomp.to_i
-	board[y] = "X"
+	board[y] = "O"
 end
 
 def compters_turn(board)
 	m = empty_boxes(board)
 	n = m[rand(m.length)]
-	board[n] = "O"
+	board[n] = "X"
 end
 
 def decide_winner(board)
 	winning_lines = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
 
 	winning_lines.each do |line|
-		line.each do |a|
-			if board.all[a] == 'X' 
-				puts "Player is the winner"
-				exit 			
-			elsif board.all[a] == "O"
-				puts "Computer is the winner"
-				exit	
-			else
-				nil
-			end
+		if board[line[0]] == 'X' && board[line[1]] == 'X' && board[line[2]] == 'X' 
+			puts "Computer is the winner"
+			exit 			
+		elsif board[line[0]] == 'O' && board[line[1]] == 'O' && board[line[2]] == 'O' 
+			puts "Player is the winner"
+			exit	
+		else
+			nil
 		end
 	end
 end
 
-
-# 			if b[line[0]] == "X" and b[line[1]] == "X" and b[line[2]] == "X"
-# 				puts "Player is the winner"
-# 				exit 
-# 			elsif b[line[0]] == "O" and b[line[1]] == "O" and b[line[2]] == "O"
-# 				puts "Computer is the winner"
-# 				exit
-# 			else
-# 				return nil
-# 			end
-# 	end
-# end
-
-draw_board(board)
-
 while empty_boxes(board).length != 0 do
+	draw_board(board)
 	players_turn(board)
 	draw_board(board)
 	decide_winner(board)
